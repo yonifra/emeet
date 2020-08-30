531e3f68cf025de8ae4b6bdf0b531a94e2d6b5b2
Jonathan Fraimorice <yonifra@gmail.com>
2020-08-30 09:40:46 +0300

    Initial commit

---
+++ Card.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import {
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    CardSubtitle,
+    CardText,
+    CardTitle
+  } from "sveltestrap";
+  import Button from "./Button.svelte";
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</svelte:head>
+
+<Card class="mb-3">
+  <CardHeader>
+    <CardTitle>Card title</CardTitle>
+  </CardHeader>
+  <CardBody>
+    <CardSubtitle>Card subtitle</CardSubtitle>
+    <CardText>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </CardText>
+    <Button/>
+  </CardBody>
+  <CardFooter>Footer</CardFooter>
+</Card>
