00e3e40630ae64eac7db84e8bfdae888f2e480f8
Jonathan Fraimorice <yonifra@gmail.com>
2020-08-31 00:23:50 +0300

    Added nice layouting

--- Category.svelte
+++ Category.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-  import { Col, Container, Row } from "sveltestrap";
-  import Card from "./Card.svelte";
+  import { Col, Container, Row } from 'sveltestrap'
+  import Card from './Card.svelte'
 </script>
 
 <Container >
-<Card />
-<Card />
-<Card />
-<Card />
-<Card />
-<Card />
-<Card />
-<Card />
-<Card />
-<Card />
+ <Row>
+    <Col><Card title="Yoga"/></Col>
+    <Col><Card title="Empowerment"/></Col>
+    <Col><Card /></Col>
+    <Col><Card /></Col>
+  </Row>
+   <Row>
+    <Col><Card /></Col>
+    <Col><Card /></Col>
+    <Col><Card /></Col>
+    <Col><Card /></Col>
+  </Row>
 </Container>
